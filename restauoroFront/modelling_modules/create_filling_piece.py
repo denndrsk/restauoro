bee03ef5fa1c2ca522bdbeb75b7f21c67a03081e
Denis Dorskiy <denisdorskiy03@MacBook-Pro-von-Denis-2.local>
2025-01-24 21:32:22 +0100

    Initial commit with .gitignore

--- restauoroFront/modelling_modules/create_filling_piece.py
+++ restauoroFront/modelling_modules/create_filling_piece.py
@@ -1,7 +1,6 @@
 import trimesh
 import numpy as np
 
-
 def create_filling_piece(clean_model_output_path, filling_Piece_model_output_path, filling_dimensions, filling_position):
     # Lade das Tür-Modell
     door_mesh = trimesh.load_mesh(clean_model_output_path)
@@ -9,8 +8,14 @@ def create_filling_piece(clean_model_output_path, filling_Piece_model_output_pat
     # Erstelle das Quader-Füllstück
     filling = trimesh.creation.box(filling_dimensions)
     
+    # Berechne die Verschiebung, um die linke untere Ecke auf filling_position zu setzen
+    shift_x = filling_position[0] + (filling_dimensions[0] / 2)
+    shift_y = filling_position[1] + (filling_dimensions[1]/2)
+    shift_z = filling_position[2] + (filling_dimensions[2] / 2)
+   
+    
     # Verschiebe das Füllstück in die richtige Position
-    filling.apply_translation(filling_position)
+    filling.apply_translation([shift_x, shift_y, shift_z])
     
     # Berechne die Differenz zwischen Füllstück und Türmodell
     filling_piece = filling.difference(door_mesh)
--- restauoroFront/products/views.py
+++ restauoroFront/products/views.py
@@ -88,7 +88,8 @@ def step2_5_selection(request, project_id):
     start_coordinates_z = request.POST.get('start_coordinates_z')
     end_coordinates_x = request.POST.get('end_coordinates_x')
     end_coordinates_z = request.POST.get('end_coordinates_z')
-    
+    minY = request.POST.get('minY')
+    maxY = request.POST.get('maxY')
     # Übergabe von project_id und door_thickness an das Template
     return render(request, 'products/step2_5_selection.html', {
         'model_url': model_url,
@@ -98,7 +99,9 @@ def step2_5_selection(request, project_id):
         'start_coordinates_x' : start_coordinates_x,
         'start_coordinates_z' : start_coordinates_z,
         'end_coordinates_x' : end_coordinates_x,
-        'end_coordinates_z' : end_coordinates_z
+        'end_coordinates_z' : end_coordinates_z,
+        'maxY' : maxY,
+        'minY' : minY
     })
     
    
@@ -131,9 +134,12 @@ def step3_clean_up(request, project_id):
     start_coordinates_z = request.POST.get('start_coordinates_z')
     end_coordinates_x = request.POST.get('end_coordinates_x')
     end_coordinates_z = request.POST.get('end_coordinates_z')
+    minY = float(request.POST.get('minY'))
+    maxY = float(request.POST.get('maxY'))
 
     print(start_coordinates_x,start_coordinates_z,end_coordinates_x,end_coordinates_z)
-
+    request.session['minY'] = minY
+    request.session['maxY'] = maxY
     request.session['door_thickness'] = door_thickness
     request.session['scale_factor'] = scale_factor
     request.session['start_coordinates_x'] = start_coordinates_x
@@ -141,7 +147,7 @@ def step3_clean_up(request, project_id):
     request.session['end_coordinates_x'] = end_coordinates_x
     request.session['end_coordinates_z'] = end_coordinates_z
     scaled_model_path = os.path.join(project_folder, f'raw_model_{project_id}_scaled.stl')
-    
+    print(minY, maxY)
     scale_model(raw_model_path,scale_factor, scaled_model_path)
 
     # Blender-Skript-Pfad
@@ -183,7 +189,9 @@ def step3_clean_up(request, project_id):
         'start_coordinates_x' : start_coordinates_x,
         'start_coordinates_z' : start_coordinates_z,
         'end_coordinates_x' : end_coordinates_x,
-        'end_coordinates_z' : end_coordinates_z
+        'end_coordinates_z' : end_coordinates_z,
+        'maxY' : maxY,
+        'minY' : minY
     })
 
 
@@ -197,16 +205,33 @@ def step3_clean_up(request, project_id):
 
 
 def step4_filling_piece(request, project_id):
+    #für trimesh
+    processed_model_url = os.path.join(settings.MEDIA_ROOT, f'project_{project_id}/processed/clean_model.stl')
+    filling_peace_url = os.path.join(settings.MEDIA_ROOT, f'project_{project_id}/processed/filling_peace.stl')
+
+
     scale_factor = float(request.session.get('scale_factor'))
-    processed_model_url = f"{settings.MEDIA_URL}/project_{project_id}/processed/processed/clean_model.stl"
-    door_thickness = float(request.session.get('door_thickness', 0.1)) / 100  # Umrechnung von cm in Meter
-    start_coordinates_x = float(request.session.get('start_coordinates_x', 0))/ scale_factor
-    start_coordinates_z = float(request.session.get('start_coordinates_z', 0))/ scale_factor
-    end_coordinates_x = float(request.session.get('end_coordinates_x', 0))/ scale_factor
-    end_coordinates_z = float(request.session.get('end_coordinates_z', 0))/ scale_factor
-   
-    filling_peace_url = os.path.join('media', f'project_{project_id}/processed/filling_peace.stl')
+    door_thickness = float(request.session.get('door_thickness', 0.1))
+    start_coordinates_x = float(request.session.get('start_coordinates_x', 0)) * scale_factor
+    start_coordinates_z = float(request.session.get('start_coordinates_z', 0))* scale_factor
+    end_coordinates_x = float(request.session.get('end_coordinates_x', 0)) * scale_factor
+    end_coordinates_z = float(request.session.get('end_coordinates_z', 0)) * scale_factor
     
+    maxY = float(request.session.get('maxY', 0))*scale_factor
+    minY = float(request.session.get('minY', 0))*scale_factor
+    print("scale by:    ", scale_factor)
+    print("start coords*scaled", start_coordinates_x,start_coordinates_z)
+    print("minY:", minY)
+    print("maxY:", maxY)
+    print("end coords*scaled", end_coordinates_x,end_coordinates_z)
+    print(door_thickness)
+    
+    
+    #für threejs
+    model_url = f"{settings.MEDIA_URL}project_{project_id}/processed/clean_model.stl"
+    threejs_filling_peace_url = f"{settings.MEDIA_URL}project_{project_id}/processed/filling_peace.stl"
+
+
     # Berechnung der Dimensionen des Füllstücks
     filling_dimensions = (
         abs(end_coordinates_x - start_coordinates_x),  # Breite des Rechtecks
@@ -217,7 +242,7 @@ def step4_filling_piece(request, project_id):
     # Berechnung der Position des Füllstücks
     filling_position = (
         min(start_coordinates_x, end_coordinates_x),  # X-Position
-        0,  # Y-Position bleibt 0 (falls keine weitere Logik benötigt wird)
+        minY, 
         min(start_coordinates_z, end_coordinates_z)  # Z-Position
     )
     create_filling_piece(processed_model_url, filling_peace_url, filling_dimensions, filling_position)
@@ -227,9 +252,9 @@ def step4_filling_piece(request, project_id):
 
 
     return render(request, 'products/step4_filling_piece.html', {
-        'model_url': processed_model_url,
+        'model_url': model_url,
         'project_id': project_id,
-        'filling_peace_url': filling_peace_url,
+        'filling_peace_url': threejs_filling_peace_url,
         'door_thickness': door_thickness,
         'scale_factor': scale_factor,
         'start_coordinates_x' : start_coordinates_x,
@@ -248,9 +273,9 @@ def step4_filling_piece(request, project_id):
 
 # Schritt 5: Bestätigung und Abschluss
 def step5_filling_peace_viewer(request,project_id):
-    if request.method == 'POST':
-        if 'confirm' in request.POST:
-            return JsonResponse({'status': 'success', 'message': 'Wizard abgeschlossen!'})
-        elif 'retry' in request.POST:
-            return redirect('step1_upload')
-    return render(request, 'products/step5_filling_peace_viewer.html', {'project_id': project_id})
+    model_url = f"{settings.MEDIA_URL}project_{project_id}/processed/clean_model.stl"
+    threejs_filling_peace_url = f"{settings.MEDIA_URL}project_{project_id}/processed/filling_peace.stl"
+    return render(request, 'products/step5_filling_peace_viewer.html', {
+        'model_url': model_url,
+        'project_id': project_id,
+        'filling_peace_url': threejs_filling_peace_url})
